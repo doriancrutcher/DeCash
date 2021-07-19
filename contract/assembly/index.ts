136bef2a9291d201c2be55d78ab26bd8ca9b4bcb
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2021-07-19 10:56:13 -0700

    fixed contract error

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -21,28 +21,7 @@ const Memos = new PersistentMap<string, string[]>('owner and receiver');
 const RecipientLog=new PersistentMap<string,string>('list of recipients');
 
 
-// Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
-export function getGreeting(accountId: string): string | null {
-  // This uses raw `storage.get`, a low-level way to interact with on-chain
-  // storage for simple contracts.
-  // If you have something more complex, check out persistent collections:
-  // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
-}
 
-export function setGreeting(message: string): void {
-  const account_id = Context.sender
-
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(
-    // String interpolation (`like ${this}`) is a work in progress:
-    // https://github.com/AssemblyScript/assemblyscript/pull/1115
-    'Saving greeting "' + message + '" for account "' + account_id + '"'
-  )
-
-  storage.set(account_id, message)
-}
 
 
 // Dorian's Cheat sheet 
